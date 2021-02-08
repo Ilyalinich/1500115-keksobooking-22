1844d75fcb65ed18c153c8b91f3daaf74d27980c
Ilyalinich <linich@yandex.ru>
2021-02-08 16:17:21 +0300

    правки по заданию Больше деталей

--- js/main.js
+++ js/main.js
@@ -36,14 +36,37 @@ const PHOTOS = [
 ];
 
 const ADVERTISEMENT_COUNT = 10;
-const LOCATION_X_MIN_COUNT = 35.65000;
-const LOCATION_X_MAX_COUNT = 35.70000;
-const LOCATION_Y_MIN_COUNT = 139.70000;
-const LOCATION_Y_MAX_COUNT = 139.80000;
+
+const UsersCount = {
+  MIN: 1,
+  MAX: 8,
+};
+
+const LocationCounts = {
+  X_MIN: 35.65000,
+  X_MAX: 35.70000,
+  Y_MIN: 139.70000,
+  Y_MAX: 139.80000,
+  DIGITS: 5,
+};
+
+const PriceCount = {
+  MIN: 0,
+  MAX: 100000,
+};
+
+const RoomsCount = {
+  MIN: 1,
+  MAX: 10,
+};
+
+const GuestsCount = {
+  MIN: 1,
+  MAX: 10,
+}
 
 
-
-const getRandomIntegerNumber = (min, max) => {
+const getRandomInteger = (min, max) => {
   if (typeof min !== 'number' || typeof max !== 'number') {
     throw new Error('В качестве аргументов необходимо использовать числа! Проверьте переданные в функцию аргументы!');
   }
@@ -67,7 +90,7 @@ const getRandomIntegerNumber = (min, max) => {
 };
 
 
-const getRandomFloatingPointNumber = (min, max, digitsAfterPoint) => {
+const getRandomFloat = (min, max, digitsAfterPoint) => {
   if (typeof min !== 'number' || typeof max !== 'number') {
     throw new Error('В качестве аргументов необходимо использовать числа! Проверьте переданные в функцию аргументы!');
   }
@@ -84,228 +107,85 @@ const getRandomFloatingPointNumber = (min, max, digitsAfterPoint) => {
 };
 
 
-const getRandomArrayElement = (elements) => {
-  return elements[getRandomIntegerNumber(0, elements.length - 1)];
-};
+const getRandomElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
+const getShuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
 const getRandomLengthArray = (elements) => {
-  const RandomLengthArray = new Array(getRandomIntegerNumber(0, elements.length)).fill(null).map((element, index) => {
-    return elements[index]
-  });
-
-  return RandomLengthArray
+  return getShuffleArray(elements)
+    .slice(0, getRandomInteger(0, elements.length));
 };
 
-const createAutor = () => {
+const getFeaturesResult = (randomLengthArray) => randomLengthArray.map(([key]) => key);
+
+const getDescriptionResult = (randomLengthArray) => {
+  if (randomLengthArray.length === 0) {
+    return ', но нам нечем вас удивить.'
+  }
+
+  const descriptionResults = randomLengthArray.map(([, value]) => value);
+
+  return `. У нас есть: ${descriptionResults.join(', ')}.`
+};
+
+const generateAd = () => {
+
+  const location = {
+    x: getRandomFloat(LocationCounts.X_MIN, LocationCounts.X_MAX, LocationCounts.DIGITS),
+    y: getRandomFloat(LocationCounts.Y_MIN, LocationCounts.Y_MAX, LocationCounts.DIGITS),
+  };
+
+  const typeResult = getRandomElement(REALTY_TYPES);
+
+  const randomLengthArray = getRandomLengthArray(Object.entries(FEATURES));
+
+
+  // const featuresResult = getRandomLengthArray(Object.keys(FEATURES));
+  // const getDescriptionResult = (featuresResult) => {
+  //   if (featuresResult.length === 0) {
+  //     return ', но нам нечем вас удивить.'
+  //   }
+
+  //   const descriptionResults = new Array(featuresResult.length)
+  //     .fill(null)
+  //     .map((element, index) => Object.values(FEATURES)[index]);
+
+  //   return `. У нас есть: ${descriptionResults.join(', ')}.`
+  // };
+
   return {
-    avatar: 'img/avatars/user0' + getRandomIntegerNumber(1, 8) + '.png',
+    author: {
+      avatar: `img/avatars/user0${getRandomInteger(UsersCount.MIN, UsersCount.MAX)}.png`,
+    },
+
+    offer: {
+      title: `${typeResult} ${getRandomElement(REALTY_NAMES)}`,
+      address: Object.values(location).join(', '),
+      price: getRandomInteger(PriceCount.MIN, PriceCount.MAX),
+      type: typeResult,
+      rooms: getRandomInteger(RoomsCount.MIN, RoomsCount.MAX),
+      guests: getRandomInteger(GuestsCount.MIN, GuestsCount.MAX),
+      checkin: getRandomElement(CHECK_TIMES),
+      checkout: getRandomElement(CHECK_TIMES),
+      features: getFeaturesResult(randomLengthArray),
+      description: `Прекрасное место для отдыха${getDescriptionResult(randomLengthArray)}`,
+      photos: getRandomLengthArray(PHOTOS),
+    },
+
+    location,
   };
 };
 
-const createOffer = () => {
-  const typeResult = getRandomArrayElement(REALTY_TYPES);
-  const featuresResult = getRandomLengthArray(Object.keys(FEATURES));
-  const getDescriptionResult = () => {
-    if (featuresResult.length === 0) {
-      return ', но нам нечем вас удивить.'
-    }
-
-    const descriptionResults = new Array(featuresResult.length).fill(null).map((element, index) => {
-      return Object.values(FEATURES)[index]
-    });
-
-    return '. У нас есть: ' + descriptionResults.join(', ') + '.'
-  };
-
-  return {
-    title: typeResult + ' ' + getRandomArrayElement(REALTY_NAMES),
-    address: '',
-    price: getRandomIntegerNumber(0, 100000),
-    type: typeResult,
-    rooms: getRandomIntegerNumber(1, 10),
-    guests: getRandomIntegerNumber(1, 10),
-    checkin: getRandomArrayElement(CHECK_TIMES),
-    checkout: getRandomArrayElement(CHECK_TIMES),
-    features: featuresResult,
-    description: 'Прекрасное место для отдыха' + getDescriptionResult(),
-    photos: getRandomLengthArray(PHOTOS),
-  };
-};
-
-const createLocation = () => {
-  return {
-    x: getRandomFloatingPointNumber(LOCATION_X_MIN_COUNT, LOCATION_X_MAX_COUNT, 5),
-    y: getRandomFloatingPointNumber(LOCATION_Y_MIN_COUNT, LOCATION_Y_MAX_COUNT, 5),
-  };
-};
-
-const totalArray = new Array(ADVERTISEMENT_COUNT).fill(null).map(() => {
-
-  const element = {
-    author: createAutor(),
-    offer: createOffer(),
-    location: createLocation(),
-  };
-
-  element.offer.address = Object.values(element.location).join(', ')
-
-  return element;
-});
+const totalArray = new Array(ADVERTISEMENT_COUNT)
+  .fill(null)
+  .map(generateAd);
 
 
 /* eslint-disable*/
 console.log(totalArray)
-
-
-// const REALTY_NAMES = [
-//   'Царские палаты',
-//   'Райское местечко',
-//   'Англетер',
-//   'Royal Resort & Spa',
-// ];
-
-// const REALTY_TYPES = [
-//   'palace',
-//   'flat',
-//   'house',
-//   'bungalow',
-// ];
-
-// const CHECK_TIMES = [
-//   '12:00',
-//   '13:00',
-//   '14:00',
-// ];
-
-// const FEATURES = {
-//   wifi: 'бесплатный wi-fi',
-//   dishwasher: 'посудомоечная машина',
-//   parking: 'собственная парковка',
-//   washer: 'стиральная машина',
-//   elevator: 'лифт',
-//   conditioner: 'кондиционер',
-// };
-
-// const PHOTOS = [
-//   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-//   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-//   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-// ];
-
-// const ADVERTISEMENT_COUNT = 10;
-// const LOCATION_X_MIN_COUNT = 35.65000;
-// const LOCATION_X_MAX_COUNT = 35.70000;
-// const LOCATION_Y_MIN_COUNT = 139.70000;
-// const LOCATION_Y_MAX_COUNT = 139.80000;
-
-
-
-// const getRandomIntegerNumber = (min, max) => {
-//   if (typeof min !== 'number' || typeof max !== 'number') {
-//     throw new Error('В качестве аргументов необходимо использовать числа! Проверьте переданные в функцию аргументы!');
-//   }
-
-//   if (min < 0 || max < 0) {
-//     throw new Error('Диапазон может быть только положительным! Проверьте переданные в функцию аргументы!');
-//   }
-
-//   if (min > max) {
-//     throw new Error('Начальное значение диапазона не может быть больше конечного! Проверьте переданные в функцию аргументы!');
-//   }
-
-//   if (max === min && !Number.isInteger(max)) {
-//     throw new Error('Аргументы равны друг другу и не являются целыми числами. Нет диапазона для поиска случайного ЦЕЛОГО числа!');
-//   }
-
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// };
-
-
-// const getRandomFloatingPointNumber = (min, max, digitsAfterPoint) => {
-//   if (typeof min !== 'number' || typeof max !== 'number') {
-//     throw new Error('В качестве аргументов необходимо использовать числа! Проверьте переданные в функцию аргументы!');
-//   }
-
-//   if (min < 0 || max < 0) {
-//     throw new Error('Диапазон может быть только положительным! Проверьте переданные в функцию аргументы!');
-//   }
-
-//   if (min > max) {
-//     throw new Error('Начальное значение диапазона не может быть больше конечного! Проверьте переданные в функцию аргументы!');
-//   }
-
-//   return parseFloat((Math.random() * (max - min) + min).toFixed(digitsAfterPoint));
-// };
-
-
-// const getRandomArrayElement = (elements) => {
-//   return elements[getRandomIntegerNumber(0, elements.length - 1)];
-// };
-
-
-// const getRandomLengthArray = (elements) => {
-//   const RandomLengthArray = new Array(getRandomIntegerNumber(0, elements.length)).fill(null).map((element, index) => {
-//     return elements[index]
-//   });
-
-//   return RandomLengthArray
-// };
-
-
-// const totalArray = new Array(ADVERTISEMENT_COUNT).fill(null).map(() => {
-
-//   const element = {
-//     author: '',
-//     offer: '',
-//     location: '',
-//   };
-
-//   element.author = {
-//     avatar: 'img/avatars/user0' + getRandomIntegerNumber(1, 8) + '.png',
-//   };
-
-//   element.location = {
-//     x: getRandomFloatingPointNumber(LOCATION_X_MIN_COUNT, LOCATION_X_MAX_COUNT, 5),
-//     y: getRandomFloatingPointNumber(LOCATION_Y_MIN_COUNT, LOCATION_Y_MAX_COUNT, 5),
-//   }
-
-//   const typeResult = getRandomArrayElement(REALTY_TYPES);
-//   const featuresResult = getRandomLengthArray(Object.keys(FEATURES));
-//   const getDescriptionResult = () => {
-//     if (featuresResult.length === 0) {
-//       return ', но нам нечем вас удивить.'
-//     }
-
-//     const descriptionResults = new Array(featuresResult.length).fill(null).map((element, index) => {
-//       return Object.values(FEATURES)[index]
-//     });
-
-//     return '. У нас есть: ' + descriptionResults.join(', ') + '.'
-//   };
-
-//   element.offer = {
-//     title: typeResult + ' ' + getRandomArrayElement(REALTY_NAMES),
-//     address: Object.values(element.location).join(', '),
-//     price: getRandomIntegerNumber(0, 100000),
-//     type: typeResult,
-//     rooms: getRandomIntegerNumber(1, 10),
-//     guests: getRandomIntegerNumber(1, 10),
-//     checkin: getRandomArrayElement(CHECK_TIMES),
-//     checkout: getRandomArrayElement(CHECK_TIMES),
-//     features: featuresResult,
-//     description: 'Прекрасное место для отдыха' + getDescriptionResult(),
-//     photos: getRandomLengthArray(PHOTOS),
-//   };
-
-//   return element;
-// });
-
-
-// /* eslint-disable*/
-// console.log(totalArray)
-
-
