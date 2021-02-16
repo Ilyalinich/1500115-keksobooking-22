c739a2ba97416cfc684546c45a1393f7b4b02aed
Ilyalinich <linich@yandex.ru>
2021-02-16 21:19:52 +0300

    Отрисуй меня полностью (часть 2)

---
+++ js/form.js
@@ -0,0 +1,36 @@
+const MIN_PRICE_COUNTS = {
+  bungalow: 0,
+  flat: 1000,
+  house: 5000,
+  palace: 10000,
+}
+
+const adForm = document.querySelector('.ad-form');
+const typeField = adForm.querySelector('#type');
+const priceField = adForm.querySelector('#price');
+
+priceField.placeholder = MIN_PRICE_COUNTS[typeField.value];
+priceField.min = MIN_PRICE_COUNTS[typeField.value];
+
+const onTypeFieldChange = () => {
+  priceField.placeholder = MIN_PRICE_COUNTS[typeField.value];
+  priceField.min = MIN_PRICE_COUNTS[typeField.value];
+}
+typeField.addEventListener('change', onTypeFieldChange);
+
+
+
+
+const timeInField = adForm.querySelector('#timein');
+const timeOutField = adForm.querySelector('#timeout');
+
+const onTimeInFieldChange = () => {
+  timeOutField.value = timeInField.value;
+}
+const onTimeOutFieldChange = () => {
+  timeInField.value = timeOutField.value;
+}
+
+timeInField.addEventListener('change', onTimeInFieldChange);
+timeOutField.addEventListener('change', onTimeOutFieldChange);
+
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import {map} from './map.js'
+import './form.js'
 
 /* eslint-disable*/
 console.log(map);
